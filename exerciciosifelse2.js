a269c2052986eccf9a73f96a1d94da89e0c5fdc4
Felipe635 <102757641+Felipe635@users.noreply.github.com>
2022-05-12 15:09:53 -0300

    Add files via upload

---
+++ exerciciosifelse2.js
@@ -0,0 +1,34 @@
+/*Exercicio 1
+
+let ano = prompt('Digite seu ano de nascimento');
+
+if(ano <= 2006){
+    alert('Você pode votar esse ano!');
+}else{
+    alert('Você não pode votar esse ano!');
+}
+
+//Exercicio 2
+
+let senha = prompt('Digite sua senha');
+
+if(senha ==  1234){
+    alert('ACESSO PERMITIDO');
+}else{
+    alert('ACESSO NEGADO');
+}*/
+
+//Exercicio 3
+
+let turno = prompt('Em qual turno você estuda? Digite M-matutino V-vespertino ou N-noturno');
+
+if(turno == 'M'){
+    alert('Bom dia!');
+} else if(turno == 'V'){
+    alert('Boa tarde!');
+} else if(turno  == 'N'){
+    alert('Boa noite!');
+}else{
+    alert('Valor inválido!');
+}
+
